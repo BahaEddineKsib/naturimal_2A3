c21cbadf0796d8bae60ce4e53ba7530515bbcb85
BahaEddineKsib <bahaeddine.ksib@esprit.tn>
2021-04-25 14:36:29 +0100

    template adopter animal

---
+++ animal/AdopterAnimal.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style/AfficheAnimalStyle.css">
+    <title>Mes Animeau</title>
+</head>
+<body>
+<div class="container">
+  <div class="images">
+    <img src="https://assets.publishing.service.gov.uk/media/600ffd8cd3bf7f05bdaef3f0/renting-pets-cockerpoo-shutterstock-960.jpg" />
+  </div>
+<!--rgb(4 169 47)-->
+  <p class="pick">MIMI est né en 06/08/2020</p>
+  
+  <div class="product">
+    <p>CHIEN</p>
+    <h1>CANICHE</h1>
+    <h2>FEMALE</h2>
+    <p class="desc">The Nike Air Zoom Pegasus 34 is a durable yet light weight running shoe. When running in these shoes, one feels a sensation that not only enhances the feeling of moving forward, but feels fun, too.</p>
+    <div class="buttons">
+      <button class="add">ADOPTER</button>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="images">
+    <img src="https://assets.publishing.service.gov.uk/media/600ffd8cd3bf7f05bdaef3f0/renting-pets-cockerpoo-shutterstock-960.jpg" />
+  </div>
+
+  <p class="pick">MIMI est né en 06/08/2020</p>
+  
+  <div class="product">
+    <p>CHIEN</p>
+    <h1>CANICHE</h1>
+    <h2>FEMALE</h2>
+    <p class="desc">The Nike Air Zoom Pegasus 34 is a durable yet light weight running shoe. When running in these shoes, one feels a sensation that not only enhances the feeling of moving forward, but feels fun, too.</p>
+    <div class="buttons">
+      <button class="add">Adopter</button>
+    </div>
+  </div>
+</div>
+</body>
+</html>
