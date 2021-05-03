109aba6e99d87a2a4a14343b428dc7fad758fd92
nardine_hanfi <nardine.hanfi@esprit.tn>
2021-05-04 00:08:37 +0100

    version 1.5 //structure Done

--- views/supprimerAccess.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "../core/animalerieC.php";
-$AccessC=new AccessC();
-if (isset($_POST["id"])){
-    $AccessC->supprimerAccess($_POST["id"]);
-    header('Location: afficher.php');
-}
-?>
--- views/supprimerAliment.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "../core/animalerieC.php";
-$AlimentC=new AlimentC();
-if (isset($_POST["id"])){
-    $AlimentC->supprimerAliment($_POST["id"]);
-    header('Location: afficher.php');
-}
-?>
