2fa30f139bacca12032dbfdbb9200da963215a8d
Sami Ben Chaabane <29543098+SamiBenChaabane@users.noreply.github.com>
2021-05-14 06:45:38 +0100

    Hebergements Client side

---
+++ views/Front/Heberg.php
@@ -0,0 +1,106 @@
+<?php
+include_once "../../Model/Hebergements.php";
+include_once "../../Controller/HebergClient/HebergC.php";
+include_once "../../Model/Rating.php";
+include_once "../../Controller/RatingClient/RatingC.php";
+require_once '../../config.php';
+$var = $_GET['var'];
+$Heberg = new Hebergements();
+$list_hebergs = $Heberg->getHebergById($var);
+$Ratings = new Hebergements();
+$list_ratings = $Ratings->getHebergByIdRATING($var);
+?>
+<!DOCTYPE html>
+
+<style>
+    @font-face {
+        font-family: 'Roboto Light';
+        src: url(fonts/Roboto-Light.ttf) format('truetype');
+    }
+
+    img {
+
+        border-radius: 10px 10px 10px 10px;
+
+    }
+
+    h1 {
+        text-align: left;
+
+    }
+
+    h2 {
+        font-family: 'Roboto Light';
+    }
+
+    .card {
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.3);
+        transition: 0.2s;
+        width: 60%;
+        border-radius: 30px;
+
+    }
+
+    .card:hover {
+        box-shadow: 0 8px 16px 0 rgba(0, 120, 215, 0.6);
+    }
+
+    .container {
+        padding: 2px 16px;
+    }
+</style>
+<?php
+require_once 'HeaderClient.php';
+require_once 'welcome.php';
+welcome("Des hebergements pour votre meilleur ami", "images/adopter.jpg");
+?>
+
+<body>
+    <?php
+    $i = 0;
+    foreach ($list_hebergs as $hebergement) :
+        if ($i != 0)
+            break;
+        $i++; ?>
+        <img src="images/681643.jpg" style="width:50%;">
+        <?php include('phpqrcode/qrlib.php');
+        $folder = "images/";
+        $file_name = "qr.png";
+        $file_name = $folder . $file_name;
+        QRcode::png($hebergement['Email'], $file_name); ?>
+        <img src="images/qr.png" width="128" height="128"></h2>
+        <h1 style="color:lightseagreen;"><?php echo $hebergement['Nom'] ?></h1>
+        <h4 style="text-align: left; display:flex; flex-direction: column; align-items: center"><?php echo $hebergement['Description']; ?></h4>
+        <h3 style="color:Gray"><?php echo $hebergement['Adresse'] ?></h3>
+        <h3 style="color:mediumseagreen"><?php echo $hebergement['Prix'];
+                                            echo "DT"; ?></h3>
+
+
+    <?php endforeach; ?>
+    <br>
+    <?php foreach ($list_ratings as $ratings) : ?>
+        <div class="card" style="width:400px;">
+            <div class="container">
+                <font color=#393e46>
+                    <h2><?php echo $ratings['Stars'];
+                        echo ' ' ?><img src="images/star.png" width="20" height="20"></h2>
+                </font>
+                <?php $RatingUsr = new Ratingc();
+                $usrrate = $RatingUsr->getUSr($ratings['User']);
+                foreach ($usrrate as $usrrt) :
+                ?>
+                    <font color="Gray">
+                        <h6><?php echo $usrrt['Email']; ?></h6>
+                    </font>
+                <?php endforeach; ?>
+                <font color="Gray">
+                    <h4><?php echo $ratings['Comment']; ?></h4>
+                </font>
+            </div>
+        </div>
+    <?php endforeach; ?>
+    <br>
+    <?php require_once 'footer.php'; ?>
+</body>
+
+</html>
