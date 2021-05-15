613220ce4977b1538a8b0141044dcf86c91e106b
yassine ben salha <yassine.bensalha@esprit.tn>
2021-05-15 13:24:17 +0100

    update

--- Controller/reclamationC.php
+++ Controller/reclamationC.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'C:/xampp/htdocs/naturimal_2A3/config1.php';
-require_once "panier.class.php";
+ require_once "C:/xampp/htdocs/naturimal_2A3/config1.php";
+ require_once "C:/xampp/htdocs/naturimal_2A3/views/Front/panier.class.php";
 require_once "C:/xampp/htdocs/naturimal_2A3/Model/reclamationM.php";
 
 $panier = new panier();
@@ -25,8 +25,9 @@ class reclamationC
     {
         $db = getConnexion();
         $ids = explode(" ", $product_id);
-        $id_commande = $ids[1];
         $id_produit = $ids[0];
+        $id_commande = $ids[1];
+        $type_article = $ids[2];
 
         try {
             $req = $db->prepare('SELECT * FROM UTILISATEUR WHERE Email =:em');
@@ -44,9 +45,10 @@ class reclamationC
         }
 
         try {
-            $req = $db->prepare('SELECT * FROM reclamation WHERE idproduit =:id AND id_commande=:id_c');
+            $req = $db->prepare('SELECT * FROM reclamation WHERE idproduit =:id AND id_commande=:id_c AND typeproduit=:tp');
             $req->bindValue(':id', $id_produit);
             $req->bindValue(':id_c', $id_commande);
+            $req->bindValue(':tp', $type_article);
             $req->execute();
             $res = $req->fetchAll(PDO::FETCH_OBJ);
         } catch (PDOException $e) {
@@ -57,7 +59,7 @@ class reclamationC
             $id = $r->idproduit;
         }
 
-        $rec = new reclamation($firstname, $lastname, $email, $id_commande, $id_produit, "jardinage");
+        $rec = new reclamation($firstname, $lastname, $email, $id_commande, $id_produit, $type_article);
         if (empty($id)) {
             try {
                 $query = $db->prepare(
@@ -83,7 +85,8 @@ class reclamationC
     {
         $db = getConnexion();
         try {
-            $req = $db->prepare('SELECT DISTINCT id_commande FROM reclamation');
+            $req = $db->prepare('SELECT DISTINCT id_commande FROM reclamation WHERE email=:email');
+            $req->bindValue(':email',$_SESSION['user']);
             $req->execute();
             $ids_commandes = $req->fetchAll(PDO::FETCH_OBJ);
         } catch (PDOException $e) {
@@ -92,12 +95,13 @@ class reclamationC
         return $ids_commandes;
     }
 
-    public function get_reclamation_for_complaints($email,$id){
+    public function get_reclamation_for_complaints($email,$id,$type_produit){
         $db = getConnexion();
         try {
-            $req = $db->prepare('SELECT * FROM reclamation WHERE email =:em AND id_commande=:id_c');
+            $req = $db->prepare('SELECT * FROM reclamation WHERE email =:em AND id_commande=:id_c AND typeproduit=:tp');
             $req->bindValue(':em', $email);
             $req->bindValue(':id_c', $id);
+            $req->bindValue(':tp', $type_produit);
             $req->execute();
             $products = $req->fetchAll(PDO::FETCH_OBJ);
           } catch (PDOException $e) {
